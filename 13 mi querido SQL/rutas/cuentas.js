fe1e2d6aa4137d2c0a2ecd44c8e509d8d0e2573b
Alberto Basalo <albertobasalo@hotmail.com>
2015-03-14 11:26:11 +0100

    13 sql

---
+++ 13 mi querido SQL/rutas/cuentas.js
@@ -0,0 +1,34 @@
+var cuentas = require('../data/cuentas.js');
+
+exports.enrutar = function (router) {
+	var rutaCuentas = router.route('/cuentas/');
+	var rutaCuentasNueva = router.route('/cuentas/nueva');
+	var rutaCuentasDni = router.route('/cuentas/dni');
+
+	rutaCuentas.get(function (peticion, respuesta) {
+		cuentas.selectAll()
+			.then(function (data) {
+				respuesta.render('cuentas', {
+					cuentas: data
+				});
+			}).fail(function (err) {
+				console.error(err);
+				respuesta.render('error', {
+					mensaje: err
+				})
+			});
+	});
+	rutaCuentasNueva.get(function (peticion, respuesta) {
+		respuesta.render('cuentas_nueva');
+	});
+
+	rutaCuentasDni
+		.get(function (peticion, respuesta) {
+			respuesta.render('cuentas_dni');
+		})
+		.post(function (peticion, respuesta) {
+			console.log(peticion.files);
+			respuesta.redirect('/cuentas');
+		});
+
+}
